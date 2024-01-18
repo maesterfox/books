2dc06bdb42de7055d63a449a915620c2d64b1a84
maesterfox <talentedmrfox@hotmail.com>
2024-01-18 23:14:23 +0000

    again..

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,7 @@ app.use(cors());
 
 app.use(
   cors({
-    origin:
-      "https://bookstore-front-1r1klzsch-maesterfoxs-projects.vercel.app/",
+    origin: "https://bookstore-front-mauve.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
